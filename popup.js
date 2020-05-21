56e5bc64ecf92626c261757f91e68b46fe7608a9
lily <lily@lilys-MBP.lan>
2020-05-21 15:41:25 +0800

    framework

---
+++ popup.js
@@ -0,0 +1,5 @@
+// 跳转至指定页面
+var btn = document.getElementById("open_url_new_tab");
+btn.addEventListener('click',function(){
+    window.open('chrome-extension://ldmhidjkjadjaljmeebmpjlamfgpnnml/homePage.html');
+},false)
